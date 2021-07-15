e253334e4b3d97b367df34a56ab42538890c260a
lustered <smellslikelavenders@gmail.com>
2021-07-15 16:51:18 -0400

    Install script

---
+++ evilinstall.sh
@@ -0,0 +1,6 @@
+# This script sets evil.sh behavior to bash 
+# Grab evil.sh file
+wget 'https://raw.githubusercontent.com/lustered/evil.sh/master/evil.sh' -P ~/
+
+# Append to bash profile 
+echo "[[ -f ~/evil.sh ]] && . ~/.evil.sh" >> ~/.bash_profile

